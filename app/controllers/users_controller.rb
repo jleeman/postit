fa887bc2afdb95971f8e0f7b45c87ad0d423b24f
Josh Leeman <joshleeman@gmail.com>
2014-05-22 23:05:46 -0400

    through show/edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
+  def show
+  end
+
   def new
     @user = User.new
   end
@@ -15,9 +21,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "#{@user.username}, you have successfully edited your profile."
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "Sorry, you don't have permission."
+      redirect_to root_path
+    else
+    end
   end
 end
