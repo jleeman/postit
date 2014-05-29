0558fecfe481fd6515fbd5e6fce6eadcb88feec0
Josh Leeman <joshleeman@gmail.com>
2014-05-29 17:45:57 -0400

    slugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 5}
+
+  # using callbacks
+  before_save :generate_slug
+
+  # default rails to_param when passing object to path is id
+  # this overrides default behaviour to use slug instead
+  def to_param
+    self.slug
+  end
+
+  def generate_slug
+    self.slug = self.username.gsub(" ", "-").downcase
+  end
+
 end
